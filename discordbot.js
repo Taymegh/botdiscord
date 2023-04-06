47d0413ff18bfe5dc4b2ec743a7c548e821c2c8f
Taymegh <73080802+Taymegh@users.noreply.github.com>
2023-04-06 09:09:37 +0200

    Add files via upload

--- discordbot.js
+++ discordbot.js
@@ -1,6 +1,9 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+const express = require('express');
+const app = express();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
